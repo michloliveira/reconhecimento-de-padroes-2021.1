761f0bb0dab9e50ee259ea2e1f571edc0678c1be
Michel Oliveira <micheltdoliveira@gmail.com>
2022-03-09 19:10:10 -0300

    add teste set

--- 1-nn-ScikitLearn.py
+++ 1-nn-ScikitLearn.py
@@ -5,9 +5,17 @@ import numpy as np
 iris = datasets.load_iris()
 x =  iris.data
 y = iris.target
-#Criando conjuntos de treino
+
+#Criando conjunto de treino
 TreinoSetosa= iris.data[:25]
 TreinoVersicolor = iris.data[50:75]
 TreinoVirginica= iris.data[100:125]
 
 Treino =  np.concatenate((TreinoSetosa,TreinoVersicolor,TreinoVirginica))
+
+#criando conjunto de teste
+TesteSetosa= iris.data[:25]
+TesteVersicolor = iris.data[50:75]
+TesteVirginica= iris.data[100:125]
+
+teste = np.concatenate((TesteSetosa,TesteVersicolor,TesteVirginica))
