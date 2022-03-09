60f48fb79e228c7845ca53f6392b2cc53650120a
Michel Oliveira <micheltdoliveira@gmail.com>
2022-03-09 19:06:55 -0300

    add treing sets

--- 1-nn-ScikitLearn.py
+++ 1-nn-ScikitLearn.py
@@ -1,3 +1,13 @@
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+
+iris = datasets.load_iris()
+x =  iris.data
+y = iris.target
+#Criando conjuntos de treino
+TreinoSetosa= iris.data[:25]
+TreinoVersicolor = iris.data[50:75]
+TreinoVirginica= iris.data[100:125]
+
+Treino =  np.concatenate((TreinoSetosa,TreinoVersicolor,TreinoVirginica))
