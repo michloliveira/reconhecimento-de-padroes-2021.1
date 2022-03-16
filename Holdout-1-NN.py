c7fd8505ac8305dff0e3f34ef91d4f2694dc916a
Michel Oliveira <micheltdoliveira@gmail.com>
2022-03-16 13:06:16 -0300

    add calculo de taxa de acerto

--- Holdout-1-NN.py
+++ Holdout-1-NN.py
@@ -37,7 +37,23 @@ def make_matriz_confusion(y_true,y_pred):
         i= i+1
     return matrix_confusion
 
+def calcular_taxa_de_acerto(matriz_confusion):
+    #A taxa de acerto do classificador é a (soma da diagonal principal)/(soma total da matriz)
 
+    #somando diagonal principal
+    soma_diagonal = matriz_confusion[0][0] + matriz_confusion[1][1] + matriz_confusion[2][2]
+    soma_total = 0
+    #somando total matriz
+    for i in range(3):
+        for j in range(3):
+            soma_total += matriz_confusion[i][j]
+    
+    print(soma_total)
+    print(soma_diagonal)
+
+    taxa_acerto = soma_diagonal / soma_total
+    print(taxa_acerto)
+    print("A taxa de acerto é {0:.2f}".format(taxa_acerto))
 
 x,y = load_wine(return_X_y = True)
 treinoX, testeX, treinoY, testeY = train_test_split(x,y,test_size=0.30) #test_size proporção 
@@ -47,8 +63,10 @@ knn.fit(treinoX,treinoY)
 
 y_pred = knn.predict(testeX)
 y_true = testeY
-print(confusion_matrix(y_true,y_pred)) #confusion_matrix(y_true, y_pred) API
+#print(confusion_matrix(y_true,y_pred)) #confusion_matrix(y_true, y_pred) API
 matriz_confusion = make_matriz_confusion(y_true,y_pred)
 print(matriz_confusion[0])
 print(matriz_confusion[1])
 print(matriz_confusion[2])
+
+calcular_taxa_de_acerto(matriz_confusion)
