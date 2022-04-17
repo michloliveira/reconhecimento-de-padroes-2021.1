20e2c1308a865c6f806c186d06899ff9fba010bf
Michel Oliveira <micheltdoliveira@gmail.com>
2022-04-16 23:41:17 -0300

    add questão 2

--- Semana-8/src/Main/Main.java
+++ Semana-8/src/Main/Main.java
@@ -36,9 +36,53 @@ public class Main {
         }
         
         
-        ImagemDigital.plotarImagem(img, "teste");
+        //ImagemDigital.plotarImagem(img, "teste");
         
         //QUESTÃO 2-------------------------------------------------------------
+        
+        int[][] square1 = new int[300][300];
+        int[][] square2 = new int[300][300];
+        int[][] square3 = new int[300][300];
+        
+        //square1
+        for(int i = 0; i < square1.length; i++){
+            for(int j = 0; j < square1.length; j++){
+                if(j >= 100 && j <= 200 && i >= 100 && i <= 200){
+                    square1[i][j] = 128;
+                }
+                else{
+                    square1[i][j] = 0;
+                }
+            }
+        }
+        //square2
+        for(int i = 0; i < square2.length; i++){
+            for(int j = 0; j < square2.length; j++){
+                if(j >= 100 && j <= 200 && i >= 100 && i <= 200){
+                    square2[i][j] = 128;
+                }
+                else{
+                    square2[i][j] = 64;
+                }
+            }
+        }
+        //square3
+        for(int i = 0; i < square3.length; i++){
+            for(int j = 0; j < square3.length; j++){
+                if(j >= 100 && j <= 200 && i >= 100 && i <= 200){
+                    square3[i][j] = 128;
+                }
+                else{
+                    square3[i][j] = 192;
+                }
+            }
+        }
+                
+        ImagemDigital.plotarImagem(square1, "Square1");
+        ImagemDigital.plotarImagem(square2, "Square2");
+        ImagemDigital.plotarImagem(square3, "Square3");
+        
+        //QUESTÃO 3-------------------------------------------------------------
     }
     
 }
