e9c1dcdbbd247400b2a0b23487959268b1c1a09f
Michel Oliveira <micheltdoliveira@gmail.com>
2022-05-10 23:20:46 -0300

    add questão 1

---
+++ Semana-11/src/main/Histograma.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.statistics.HistogramDataset;
+
+/**
+ *
+ * @author michel
+ */
+public class Histograma extends JFrame{
+    
+    public Histograma(int[] dataArray, String title){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("histograma");
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        criarHistograma(dataArray, title);
+        setVisible(true);
+    }
+    
+    public void criarHistograma(int[] dataArray, String title){
+
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        for(int i = 0; i < dataArray.length; i++){
+            dataSet.addValue(dataArray[i], "tons", (Integer) i);         
+        }
+
+        
+         
+        JFreeChart chart = ChartFactory.createBarChart(title, "",
+            "", dataSet, PlotOrientation.VERTICAL, true, false, false);
+        chart.setBackgroundPaint(Color.WHITE);
+        
+        ChartPanel painel =  new ChartPanel(chart);
+        add(painel);
+    }
+    
+    
+}
---
+++ Semana-11/src/main/Questao1.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import imagemDigital.ImagemDigital;
+
+/**
+ *
+ * @author michel
+ */
+public class Questao1 {
+    
+    static int[] calcularHistograma(int[][] img ,int[] histograma){
+        //calcular histograma
+        for(int i = 0; i < img.length; i++){
+            for(int j = 0; j < img[0].length; j++){
+                histograma[img[i][j]] += 1;
+            }
+        }
+        return histograma;
+    }
+    
+    static int[] calcularHistogramaAcumulado(int[] histograma, int[] histAcumulado){
+        histAcumulado[0] = histograma[0];
+        for(int i = 1; i < 256; i++){
+            histAcumulado[i] = histograma[i] + histAcumulado[i-1];
+        }
+        return histAcumulado;
+    }
+    static float menor(int[] histograma){
+        for(int i = 0; i < histograma.length; i++){
+            if(histograma[i] != 0){
+                return histograma[i];
+            }
+        }
+        return 0;
+    }
+    
+    static int[] calcularMapaCores(int[] mapaCores, int[] histAcumulado, int[][] img){
+        //calcular mapa de cores
+        int pixels = img.length * img[0].length;
+        float menor = menor(histAcumulado);
+        
+        for(int i = 0; i < 256; i++){
+            mapaCores[i] = (int) Math.round(((histAcumulado[i] - menor) / (pixels - menor)) * 255);
+        }
+        return histAcumulado;
+    }
+    
+    static int[][] atualizarCores(int[] mapaCores, int[][] img, int[][] out){
+        for(int i = 0; i < img.length; i++){
+            for(int j = 0; j < img[0].length; j++){
+                out[i][j] = mapaCores[img[i][j]];
+            }
+        }
+        return out;
+    }
+    
+    public static void main(String[] args) {
+        
+        for(int i = 0; i< 7; i++){
+            int[][] img = ImagemDigital.carregarImagem("./imagens/"+ i + ".png");
+            int[][] out = new int[img.length][img[0].length];
+
+
+            int histograma[] = new int[256];
+            int histAcumulado[] = new int[256];
+            int mapaCores[] = new int[256];
+
+            calcularHistograma(img, histograma);
+            calcularHistogramaAcumulado(histograma, histAcumulado);
+            calcularMapaCores(mapaCores, histAcumulado, img);
+            atualizarCores(mapaCores, img, out);
+            ImagemDigital.plotarImagem(img, "original " + i);
+            ImagemDigital.plotarImagem(out, "saida " + i);
+            new Histograma(histograma,"antes " + i);
+            new Histograma(mapaCores,"depois " + i);
+              
+        }
+        
+
+   
+        
+    }
+    
+    
+}
